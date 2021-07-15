baf20300902c6d2084c8b59b1b76161b7065385c
decaby7e <27081624+decaby7e@users.noreply.github.com>
2021-07-15 18:27:54 -0400

    streamlines

--- ci/build-home-site.sh
+++ ci/build-home-site.sh
@@ -3,19 +3,15 @@ set -e
 
 # CISE Home Website Deployment
 
-REPO='https://github.com/decaby7e/cise-jfp-site'
-tmp=`mktemp -d`
 script_path=$(dirname $(realpath -s $0))
 
 # Check for dependencies
 "$script_path/install-deps.sh" "check"
 
 # Build and setup the webroot
-hugo -s "$tmp/home-site" -d "$HOME/public_html/"
-rsync -a --delete --exclude 'files' "$tmp/home-site/public/" "$HOME/public_html/"
+hugo -s "$script_path/.." -d "$HOME/public_html/"
+rsync -a --delete --exclude 'files' "$script_path/../public" "$HOME/public_html/"
 "$script_path/gen-directory-index.py" -r "$HOME/public_html/files/"
-
-rm -rf $tmp
 
 echo "Successfully deployed site to https://cise.ufl.edu/~$USER/"
 
